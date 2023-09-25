c1558c4e8357aa297d9fefaccdab7eea07be15e6
Lucian Pezzillo <143512680+Luciantyhh@users.noreply.github.com>
2023-09-25 15:03:56 -0400

    purchase 2.5

--- Assignment 2.5/if_else_sign_practice.py
+++ Assignment 2.5/if_else_sign_practice.py
@@ -5,8 +5,16 @@ def main():
         If they're 18 and older but younger than 21, tell them they can only buy nicotine products in some states
         If they're less than 18, they can only purchase candy cigarettes and sody pops
     '''
-    
-  
+    print("how old are you")
+    require = ["you can buy alchol","you can buy nicotine","you can rent a car"]
+    age = 25
+    print(age)
+    if age >= 25:
+        print(require)
+    if age < 24:
+        print(require[0,1])
+    if age < 18:
+        print("you can only buy candy and soda")
 
   
   
