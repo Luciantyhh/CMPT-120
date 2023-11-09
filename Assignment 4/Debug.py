66b04a2a239c4b81d68ff61862be403fdd71497d
Lucian Pezzillo <143512680+Luciantyhh@users.noreply.github.com>
2023-11-09 13:58:12 -0500

    black jack

--- Assignment 4/Debug.py
+++ Assignment 4/Debug.py
@@ -1,9 +1,10 @@
 
-def printhello():
-    print("Hello")
-    
-def printName(x):
-    print(x)
+def printHello():
+    print("hello")
+    #call printName and give it the parameter of your name
+
+def printname():
+    Print("Lucian")
     
 def addition(x, y):
     #add x and y together and return them
@@ -21,22 +22,17 @@ def smaller(i, j):
     #if they're even, return 0
 def main():
     print("hello")
-    #recall
-def printHello():
-    print("hello")
-    #call printName and give it the parameter of your name
-def printname():
-    Print("Lucian")
-
-    var1= 10
-    var2= 20
-    #What do we put in here to make it work?
-    print(addition(var1 + var2))
+    var1 = 10
+    var2 = 20
+    # What do we put in here to make it work?
+    print(addition(var1, var2))
 
     num1 = int(input("Enter number 1"))
     num2 = int(input("Enter number 2"))
-    #what go here?
-    print(smaller(num1 > num2))
+    # what go here?
+    print(smaller(num1, num2))
+    #recall
+d
 
 
 
